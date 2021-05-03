8e9f5ca64d54dcfa15b2f8429d974806ef97fc55
Eduardo Schnoor <44807267+dxzin@users.noreply.github.com>
2021-05-02 22:11:24 -0300

    Delete ex05.py

--- ex05.py
+++
@@ -1,7 +0,0 @@
-#solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o 
-#preço a pagar.
-p = float(input("Produto: "))
-d = int(input("Desconto: "))
-s = (p * d / 100)
-total = p - s
-print("O produto R${} com desconto de {} = {} saira por {}".format(p,d,s,total))
