e50e8ba73fe2367d6677f386ca16a578f468f157
Eduardo Schnoor <44807267+dxzin@users.noreply.github.com>
2021-05-02 22:11:30 -0300

    Delete ex06.py

--- ex06.py
+++
@@ -1,7 +0,0 @@
-# Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média 
-#esperada para a viagem.
-
-d = int(input("Distancia KM: "))
-v = int(input("Velocidade: "))
-soma =  d / v
-print("Sua viagem vai durar em média {}horas".format(soma))
